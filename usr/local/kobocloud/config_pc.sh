32572b09a903505ca3d6a9434f996d9a00b12d1a
Felix Egli <mail@felix-egli.ch>
2020-01-20 13:09:29 +0100

    make use of $HOME

--- usr/local/kobocloud/config_pc.sh
+++ usr/local/kobocloud/config_pc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-Logs=/home/francesco/src/KoboCloud
-Lib=/home/francesco/src/KoboCloud/Library
-SD=/home/francesco/src/KoboCloud
-UserConfig=/home/francesco/src/KoboCloud/kobocloudrc
+Logs=$HOME/KoboCloud
+Lib=$HOME/KoboCloud/Library
+SD=$HOME/KoboCloud
+UserConfig=$HOME/KoboCloud/kobocloudrc
 Dt="date +%Y-%m-%d_%H:%M:%S"
 CURL=/usr/bin/curl
