e06fda5676145306975001354900ec46c2aa21eb
Felix Egli <mail@felix-egli.ch>
2024-05-18 13:15:10 +0200

    add nm_program.sh

---
+++ KoboRoot/usr/local/kobocloud/nm_program.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#load config
+. `dirname $0`/config.sh
+
+#create work dirs
+[ ! -e "$Logs" ] && mkdir -p "$Logs" >/dev/null 2>&1
+[ ! -e "$Lib" ] && mkdir -p "$Lib" >/dev/null 2>&1
+[ ! -e "$SD" ] && mkdir -p "$SD" >/dev/null 2>&1
+
+#output to log
+`dirname $0`/get.sh &> $Logs/get.log &
